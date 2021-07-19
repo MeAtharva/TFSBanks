5d573cbbccb2e0e4139d2cf329501e35dc138952
Atharva Arjun <81384722+MeAtharva@users.noreply.github.com>
2021-07-20 01:30:14 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <title>Basic Banking System</title>
+  </head>
+
+  <body>
+  <?php
+  include 'navbar.php';
+  ?>
+  
+      <!-- banner section  -->
+    <section id="banner">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <p class="promo-title">DIGITAL BANKING SYSTEM</p>
+                    <p class="para">Embracing the Future of Banking </p>
+                    <div class="content">
+                        <img src="img/card2.png">
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <h5>Enjoy banking at you fingertip...</h5></img>
+                    </div>
+
+                </div>
+                <div class="col-md-6 text-center">
+                    <img src="img/sidebg.png" class="img-fluid" alt="">
+                </div>
+            </div>
+        </div>
+        <img src="img/wave1.png" class="bottom-img" alt="">
+    </section>
+
+    <footer class="text-center mt-5 py-2">
+        <p>Copyright &copy 2021. Made with &#10084 by <b>KAJOL RATHOD</b> <br> The Sparks Foundation</p>
+    </footer>
+
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,32 @@
+       <!-- navbar section -->
+    <section id="nav-bar">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <!-- <div class="container-fluid"> -->
+            <a class="navbar-brand font-weight-bold" href="#"><img src="images/logo.png" alt="">TSF Bank</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                               Actions
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="transfermoney.php">Money Transfer</a>
+                                <a class="dropdown-item" href="transactionhistory.php">Transaction History</a>
+                            </div>
+                        </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="createuser.php">Create User</a>
+                    </li>
+                </ul>
+            </div>
+            </div>
+        </nav>
+    </section>
